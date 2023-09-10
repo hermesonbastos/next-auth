781c27253d87bf556b605a51e046c7b3aa791924
hermesonbastos <hermesonbastos@gmail.com>
2023-09-10 00:26:01 -0300

    fear: sign in as sign up with Google button

---
+++ src/components/GoogleSignInButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from './ui/button'
+import { FC, ReactNode } from 'react'
+
+interface GoogleSignInButtonProps {
+  children: ReactNode,
+}
+
+const GoogleSignInButton: FC<GoogleSignInButtonProps> = ({ children }) => {
+  const loginWithGoogle = () => console.log('login with google')
+  return <Button onClick={loginWithGoogle} className='w-full'>{children}</Button>
+}
+
+export default GoogleSignInButton
